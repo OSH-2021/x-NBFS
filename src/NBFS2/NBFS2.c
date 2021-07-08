d9f8cfd1986c6074c599851686f02ca399fb0ee8
C Hu@ng <1457043769@qq.com>
2021-07-08 20:54:51 +0800

    NBFS2 code

--- src/NBFS2/NBFS2.c
+++ src/NBFS2/NBFS2.c
@@ -51,7 +51,6 @@ struct _nbfs2_file
     struct _nbfs2_filesystem *fs;
     spdk_blob_id blobid;
     char *filename;
-    void *data;
 };
 struct _nbfs2_args
 {
@@ -60,22 +59,64 @@ struct _nbfs2_args
     enum FS_BUILD_METHOD build_mthd;
     char *bdev_name;
     struct spdk_bs_dev *bs_dev;
+    spdk_blob_op_complete cb_fn;
+    void *cb_arg;
 };
-static void nbfs2_read_file(struct _nbfs2_args *args, void *buffer,
-                            uint64_t length)
+static void _nbfs2_blob_deleted(void *cb_arg, int bserrno)
+{
+    //this function runs after spdk_bs_delete_blob
+    SPDK_NOTICELOG("entry\n");
+    struct _nbfs2_args *args = cb_arg;
+    //errno handle
+    if (bserrno)
+    {
+        SPDK_ERRLOG("Error %d creating a blob\n", bserrno);
+        spdk_app_stop(-1);
+        return;
+    }
+    //remove hash table item
+    char blobid_string[BLOBID_STRING_LEN];
+    blobid2char(args->file->blobid, blobid_string);
+    ht_delete(args->fs->table, blobid_string);
+    //update data size
+    args->fs->spb_data_size -= sizeof(struct _nbfs2_super_block_meta_data);
+    //file deleted
+    SPDK_NOTICELOG("file deleted!\n");
+}
+static void nbfs2_delete_file(struct _nbfs2_args *args, char *filename,
+                              spdk_blob_op_complete cb_fn, void *cb_arg)
+{
+    //delete file
+    //set callback fn&arg
+    args->cb_fn = cb_fn;
+    args->cb_arg = cb_arg;
+    //search table and get blobid
+    char blobid_string[BLOBID_STRING_LEN];
+    strcpy(blobid_string, ht_search(args->fs->table, filename));
+    spdk_blob_id blobid;
+    char2blobid(blobid_string, &blobid);
+    //set file object
+    args->file->filename = filename;
+    args->file->blobid = blobid;
+    args->file->fs = args->fs;
+    //delete blob
+    spdk_bs_delete_blob(args->fs->bs, blobid, _nbfs2_blob_deleted, args);
+}
+static void nbfs2_read_file(struct _nbfs2_args *args, void *buffer,uint64_t length,
+                              spdk_blob_op_complete cb_fn, void *cb_arg)
 {
     //read file to buffer(must be opened)
     read_normal_blob(args->fs->bs, args->file->blobid, buffer,
-                     ENABLE_RANDOM_RW, length, NULL, NULL);
+                     ENABLE_RANDOM_RW, length, cb_fn, cb_arg);
 }
-static void nbfs2_write_file(struct _nbfs2_args *args, void *buffer,
-                             uint64_t length)
+static void nbfs2_write_file(struct _nbfs2_args *args, void *buffer,uint64_t length,
+                              spdk_blob_op_complete cb_fn, void *cb_arg)
 {
     //write buffer to file(must be opened)
     write_normal_blob(args->fs->bs, args->file->blobid, buffer,
-                      ENABLE_RANDOM_RW, length, NULL, NULL);
+                      ENABLE_RANDOM_RW, length, cb_fn, cb_arg);
 }
-static void nbfs2_open_file(struct _nbfs2_args *args, char *filename)
+static void nbfs2_open_file_sync(struct _nbfs2_args *args, char *filename)
 {
     //use filename open file
     //search table and get blobid
@@ -99,6 +140,7 @@ static void _nbfs2_file_created(void *cb_arg, spdk_blob_id blobid, int bserrno)
     if (bserrno)
     {
         SPDK_ERRLOG("Error %d creating a blob\n", bserrno);
+        args->cb_fn(args->cb_arg,bserrno);
         spdk_app_stop(-1);
         return;
     }
@@ -113,14 +155,20 @@ static void _nbfs2_file_created(void *cb_arg, spdk_blob_id blobid, int bserrno)
     args->fs->spb_data_size += sizeof(struct _nbfs2_super_block_meta_data);
     //file created
     SPDK_NOTICELOG("file created!\n");
+    args->cb_fn(args->cb_arg,0);
 }
-static void nbfs2_create_file(struct _nbfs2_args *args, char *filename)
+static void nbfs2_create_file(struct _nbfs2_args *args, char *filename,
+        spdk_blob_op_complete 	cb_fn,void * 	cb_arg )
 {
     //create a file
+    //set callback fn&arg
+    args->cb_fn = cb_fn;
+    args->cb_arg = cb_arg;
     struct _nbfs2_file *file;
     if ((file = malloc(sizeof(struct _nbfs2_file))) == NULL)
     {
         SPDK_ERRLOG("Error %d malloc a file\n", -ENOMEM);
+        args->cb_fn(args->cb_arg,-ENOMEM);
         spdk_app_stop(-1);
         return;
     }
@@ -140,6 +188,7 @@ static void _nbfs2_unload_complete(void *cb_arg, int bserrno)
     if (bserrno)
     {
         SPDK_ERRLOG("Error %d unload the blobstore\n", bserrno);
+        args->cb_fn(args->cb_arg,bserrno);
         spdk_app_stop(-1);
         return;
     }
@@ -147,6 +196,7 @@ static void _nbfs2_unload_complete(void *cb_arg, int bserrno)
     spdk_free(args->fs->write_buff);
     spdk_free(args->fs->read_buff);
     //return
+        args->cb_fn(args->cb_arg,0);
     spdk_app_stop(0);
 }
 static void _nbfs2_superblob_written(void *cb_arg, int bserrno)
@@ -158,6 +208,7 @@ static void _nbfs2_superblob_written(void *cb_arg, int bserrno)
     if (bserrno)
     {
         SPDK_ERRLOG("Error %d writing the superblob\n", bserrno);
+        args->cb_fn(args->cb_arg,bserrno);
         spdk_app_stop(-1);
         return;
     }
@@ -172,6 +223,7 @@ static void _nbfs2_write_htable(struct _nbfs2_args *args)
     if ((spb_mdata = malloc(sizeof(struct _nbfs2_super_block_meta_data))) == NULL)
     {
         SPDK_ERRLOG("Error %d malloc spb_mdata\n", -ENOMEM);
+        args->cb_fn(args->cb_arg,-ENOMEM);
         spdk_app_stop(-1);
         return;
     }
@@ -198,7 +250,7 @@ static void _nbfs2_write_htable(struct _nbfs2_args *args)
                 {
                     mdata_num++;
                     //set spb_mdata
-                    strcpy(spb_mdata->name,head->item->key);
+                    strcpy(spb_mdata->name, head->item->key);
                     char2blobid(head->item->value, &(spb_mdata->blobid));
                     //write it to buffer
                     memcpy(args->fs->write_buff + offset, spb_mdata,
@@ -222,9 +274,13 @@ static void _nbfs2_write_htable(struct _nbfs2_args *args)
     write_super_blob(args->fs->bs, args->fs->write_buff, 0,
                      args->fs->spb_data_size, _nbfs2_superblob_written, args);
 }
-static void nbfs2_unload_fs(struct _nbfs2_args *args)
+static void nbfs2_unload_fs(struct _nbfs2_args *args,
+                              spdk_blob_op_complete cb_fn, void *cb_arg)
 {
     //unload filesystem
+    //set callback fn&arg
+    args->cb_fn = cb_fn;
+    args->cb_arg = cb_arg;
     //store hash table
     //malloc write buffer
     args->fs->write_buff = spdk_malloc(args->fs->spb_data_size,
@@ -242,6 +298,7 @@ static void _nbfs2_build_htable(struct _nbfs2_args *args)
     if ((spb_mdata = malloc(sizeof(struct _nbfs2_super_block_meta_data))) == NULL)
     {
         SPDK_ERRLOG("Error %d malloc spb_mdata\n", -ENOMEM);
+        args->cb_fn(args->cb_arg,-ENOMEM);
         spdk_app_stop(-1);
         return;
     }
@@ -265,6 +322,7 @@ static void _nbfs2_build_htable(struct _nbfs2_args *args)
     }
     //hash table created
     SPDK_NOTICELOG("hash table loaded!\n");
+    args->cb_fn(args->cb_arg,0);
 }
 static void _nbfs2_superblob_read(void *cb_arg, int bserrno)
 {
@@ -275,6 +333,7 @@ static void _nbfs2_superblob_read(void *cb_arg, int bserrno)
     if (bserrno)
     {
         SPDK_ERRLOG("Error %d reading the superblob\n", bserrno);
+        args->cb_fn(args->cb_arg,bserrno);
         spdk_app_stop(-1);
         return;
     }
@@ -290,6 +349,7 @@ static void _nbfs2_superblob_opened_load(void *cb_arg, struct spdk_blob *blb, in
     if (bserrno)
     {
         SPDK_ERRLOG("Error %d openning the superblob\n", bserrno);
+        args->cb_fn(args->cb_arg,bserrno);
         spdk_app_stop(-1);
         return;
     }
@@ -324,17 +384,18 @@ static void _nbfs2_superblob_id_got(void *cb_arg, spdk_blob_id blobid, int bserr
     if (bserrno)
     {
         SPDK_ERRLOG("Error %d getting the superblob id\n", bserrno);
+        args->cb_fn(args->cb_arg,bserrno);
         spdk_app_stop(-1);
         return;
     }
     //set id
     args->fs->superblob_id = blobid;
     //open superblob
-    spdk_bs_open_blob(args->fs->bs,args->fs->superblob_id,
-    _nbfs2_superblob_opened_load,args);
+    spdk_bs_open_blob(args->fs->bs, args->fs->superblob_id,
+                      _nbfs2_superblob_opened_load, args);
 }
 static void _nbfs2_bs_load_complete(void *cb_arg, struct spdk_blob_store *bs,
-                              int bserrno)
+                                    int bserrno)
 {
     //this function runs after spdk_bs_load
     SPDK_NOTICELOG("entry\n");
@@ -343,6 +404,7 @@ static void _nbfs2_bs_load_complete(void *cb_arg, struct spdk_blob_store *bs,
     if (bserrno)
     {
         SPDK_ERRLOG("Error %d loading the blobstore\n", bserrno);
+        args->cb_fn(args->cb_arg,bserrno);
         spdk_app_stop(-1);
         return;
     }
@@ -352,7 +414,8 @@ static void _nbfs2_bs_load_complete(void *cb_arg, struct spdk_blob_store *bs,
     //get superblob id
     spdk_bs_get_super(args->fs->bs, _nbfs2_superblob_id_got, args);
 }
-static void _nbfs2_superblob_opened_init(void *cb_arg, struct spdk_blob *blb, int bserrno){
+static void _nbfs2_superblob_opened_init(void *cb_arg, struct spdk_blob *blb, int bserrno)
+{
     //this function runs after spdk_bs_open_blob
     SPDK_NOTICELOG("entry\n");
     struct _nbfs2_args *args = cb_arg;
@@ -360,6 +423,7 @@ static void _nbfs2_superblob_opened_init(void *cb_arg, struct spdk_blob *blb, in
     if (bserrno)
     {
         SPDK_ERRLOG("Error %d open superblob\n", bserrno);
+        args->cb_fn(args->cb_arg,bserrno);
         spdk_app_stop(-1);
         return;
     }
@@ -374,8 +438,10 @@ static void _nbfs2_superblob_opened_init(void *cb_arg, struct spdk_blob *blb, in
     args->fs->table = create_table(args->fs->ht_size);
     //show
     SPDK_NOTICELOG("nbfs2 initialized!\n");
+    args->cb_fn(args->cb_arg,0);
 }
-static void _nbfs2_superblob_created(void *cb_arg, spdk_blob_id blobid, int bserrno){
+static void _nbfs2_superblob_created(void *cb_arg, spdk_blob_id blobid, int bserrno)
+{
     //this function runs after create_superblob
     SPDK_NOTICELOG("entry\n");
     struct _nbfs2_args *args = cb_arg;
@@ -383,17 +449,18 @@ static void _nbfs2_superblob_created(void *cb_arg, spdk_blob_id blobid, int bser
     if (bserrno)
     {
         SPDK_ERRLOG("Error %d initalize the blobstore\n", bserrno);
+        args->cb_fn(args->cb_arg,bserrno);
         spdk_app_stop(-1);
         return;
     }
     //set blobid
     args->fs->superblob_id = blobid;
     //open superblob
-    spdk_bs_open_blob(args->fs->bs, args->fs->superblob_id, 
-    _nbfs2_superblob_opened_init, args);
+    spdk_bs_open_blob(args->fs->bs, args->fs->superblob_id,
+                      _nbfs2_superblob_opened_init, args);
 }
 static void _nbfs2_bs_init_complete(void *cb_arg, struct spdk_blob_store *bs,
-                              int bserrno)
+                                    int bserrno)
 {
     //this function runs after spdk_bs_init
     SPDK_NOTICELOG("entry\n");
@@ -402,6 +469,7 @@ static void _nbfs2_bs_init_complete(void *cb_arg, struct spdk_blob_store *bs,
     if (bserrno)
     {
         SPDK_ERRLOG("Error %d initalize the blobstore\n", bserrno);
+        args->cb_fn(args->cb_arg,bserrno);
         spdk_app_stop(-1);
         return;
     }
@@ -411,8 +479,13 @@ static void _nbfs2_bs_init_complete(void *cb_arg, struct spdk_blob_store *bs,
     //create superblob
     create_super_blob(args->fs->bs, _nbfs2_superblob_created, args);
 }
-static void nbfs2_build_fs(struct _nbfs2_args *args){
+static void nbfs2_build_fs(struct _nbfs2_args *args,
+                              spdk_blob_op_complete cb_fn, void *cb_arg)
+{
     //use this function to build fs
+    //set callback fn&arg
+    args->cb_fn = cb_fn;
+    args->cb_arg = cb_arg;
     if (args->build_mthd == MTHD_INIT)
     {
         //initialize
@@ -430,7 +503,8 @@ static void _test_bdev_event_cb(enum spdk_bdev_event_type type, struct spdk_bdev
     //this function is a bdev event callback
     SPDK_WARNLOG("Unsupported bdev event: type %d\n", type);
 }
-static void _nbfs2_start(void *arg1){
+static void _nbfs2_start(void *arg1)
+{
     //this function runs after spdk_app_start
     SPDK_NOTICELOG("entry\n");
     struct _nbfs2_args *args = arg1;
@@ -449,9 +523,10 @@ static void _nbfs2_start(void *arg1){
     //set bs_dev
     args->bs_dev = bs_dev;
     //build blobstore
-    nbfs2_build_fs(args);
+    nbfs2_build_fs(args,NULL,NULL);
 }
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
     if (argc != CMD_ARG_NUM)
     {
         SPDK_ERRLOG("Error %d invalid argument\n", -EINVAL);
